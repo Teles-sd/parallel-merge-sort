a84354c48f0ecf752cce30e7f1c3d1320c0bb6c9
Teles-sd <teles.ltds@gmail.com>
2025-02-26 11:57:58 -0300

    Merge-Sort implemented.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Requisitos
 
-- [ ] Implementação sequencial ([seqms.c](./seqms.c)).
+- [x] Implementação sequencial ([seqms.c](./seqms.c)).
 - [ ] Implementação paralela ([parms.c](./parms.c)).
 - [ ] Testar métricas de desempenho com 1, 2, 4, 8 e 12 _threads_ (se disponíveis no sistema).
   - _Speedup_, eficiência e desempenho.
--- seqms.c
+++ seqms.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>    // atoi, rand
 
+// Sort (in ascending order) a randomly generated array of integers, using Merge-Sort algorithm.
+
 void print_help(void);
 void displayArray();
 
-int main(int argc, char *argv[]) {
+void arrMerge();
+void arrDivide();
+void mergeSort();
+
+int main(int argc, char* argv[]) {
 
     // No extra command line argument passed
     if (argc != 2) {
@@ -26,7 +32,7 @@ int main(int argc, char *argv[]) {
     for (int i=0; i<arraySize; i++)
         array[i] = i;
 
-    printf("Array before randomizing.\n");
+    printf("Array before randomizing:\n");
     displayArray(array, arraySize);
     printf("\n");
     
@@ -39,7 +45,12 @@ int main(int argc, char *argv[]) {
         array[i] = array[randomIndex];
         array[randomIndex] = aux;
     }
-    printf("Array after randomizing.\n");
+    printf("Array after randomizing:\n");
+    displayArray(array, arraySize);
+
+    // Sorting
+    mergeSort(array, arraySize);                    // To-do: include print of sorting process
+    printf("Array after sorting:\n");
     displayArray(array, arraySize);
 
     return 0;
@@ -72,4 +83,71 @@ void displayArray(int *arr, int arrSize) {
         }
     }
     printf("]\n");
+}
+
+// Merge two subarrays of arr in order
+// Internal function of mergeSort; should no be called in main
+void arrMerge(int arr[], int indexL, int indexM, int indexR) {
+    
+    // subarrays are:
+    // arrAux1[indexL .. indexM]
+    // arrAux2[indexM+1..indexR]
+    
+    // Auxiliary arrays
+    int arrAuxSize1 = (indexM - indexL) + 1;
+    int arrAuxSize2 =  indexR - indexM;
+    int arrAux1[arrAuxSize1], arrAux2[arrAuxSize2];
+
+    for (int i = 0; i < arrAuxSize1; i++)
+        arrAux1[i] = arr[indexL + i];
+    for (int i = 0; i < arrAuxSize2; i++)
+        arrAux2[i] = arr[indexM + i + 1];
+
+    // Merging auxiliar arrays orderly into arr
+    int i, r, l;
+    i = indexL;
+    r = l = 0;
+    while (r < arrAuxSize1 && l < arrAuxSize2) {
+        if (arrAux1[r] <= arrAux2[l]) {
+            arr[i] = arrAux1[r];
+            r++;
+        } else {
+            arr[i] = arrAux2[l];
+            l++;
+        }
+        i++;
+    }
+
+    // Copying the remaining elements, if any
+    while (r < arrAuxSize1) {
+        arr[i] = arrAux1[r];
+        r++;
+        i++;
+    }
+    while (l < arrAuxSize2) {
+        arr[i] = arrAux2[l];
+        l++;
+        i++;
+    }
+}
+
+// Recursive calls dividing the array into subarrays
+// Internal function of mergeSort; should no be called in main
+void arrDivide(int arr[], int indexL, int indexR) {
+
+    // If the indexes are the same, the subarray has only one element, and thus, is sorted
+    if (indexL < indexR) {
+
+        int indexM = indexL + (indexR - indexL) / 2;
+
+        arrDivide(arr, indexL, indexM);
+        arrDivide(arr, indexM + 1, indexR);
+
+        arrMerge(arr, indexL, indexM, indexR);
+    }
+}
+
+void mergeSort(int *arr, int arrSize) {
+
+    arrDivide(arr, 0, arrSize-1);
 }
